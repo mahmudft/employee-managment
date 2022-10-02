329d341545b1c7d33c508b9610ff18bc289ebb88
Mahmud <fatullayevm@gmail.com>
2022-10-02 14:12:54 +0400

    admin panel adding models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from api.models import Employee, Team, TeamLeader, WorkTime
+
 # Register your models here.
+
+
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+class TeamLeaderAdmin(admin.ModelAdmin):
+    pass
+
+
+class WorkTimeAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(TeamLeader, TeamLeaderAdmin)
+admin.site.register(WorkTime, WorkTimeAdmin)
